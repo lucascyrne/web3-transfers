4efd46450b38da980966d0be737f07569fca6e75
lucascyrne <cyrne24@gmail.com>
2022-12-19 13:22:23 -0300

    feat: erc20 transfers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
+import { MetaMaskInpageProvider } from '@metamask/providers';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+
+declare global {
+  interface Window {
+    ethereum?: MetaMaskInpageProvider;
+  }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
